5b48b9267ee59f679c62ed4ac779b13ec1b40553
github username <aude53@gmail.com>
2019-08-29 17:53:42 +0100

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,16 @@
 class ArtistsController < ApplicationController
+
+    get '/artists' do
+    # This should present the user with a list of all artists in the library.
+    # Each artist should be a clickable link to that particular artist's show page.
+        @artists = Artist.all
+        erb :'artists/index'
+    end
+
+    get '/artists/:slug' do
+    # Any given artist's show page should have links to each of his or her songs and genres.
+        @artist = Artist.find_by_slug(params[:slug])
+        erb :'/artists/show'
+    end
+
 end
